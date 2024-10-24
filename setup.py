ed0ba35321f46f338b1503506f9713dee297c3c1
Keenan Chen <110057425+kcinbk@users.noreply.github.com>
2024-10-24 18:38:34 -0400

    initiated setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='MyStar',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Keenan Chen',
+      packages=[
+        'numpy', 
+        'pandas', 
+        'requests',
+        'bs4',
+        'time',
+        'random',
+        'json',
+        'tqdm',
+        're'
+                ]
+      )
